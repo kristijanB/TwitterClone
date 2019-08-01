6aa69a6b41a39dcf09441c88c4294fb498f2fa1e
Kristijan Brechelmacher <kristijanB@users.noreply.github.com>
2019-08-01 21:24:54 +0200

    dodan login bez funkcionalnosti

--- app/src/main/java/com/example/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.twitterclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class   LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+    }
+    fun onLogin(v: View){
+
+    }
+    fun goToSignup(v: View){
+
     }
 }
